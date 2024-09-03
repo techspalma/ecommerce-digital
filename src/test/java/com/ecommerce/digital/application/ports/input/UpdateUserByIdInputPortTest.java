6ce21b7b239e69f32d6d93f9d84f5b8478f860f7
maran <maranmorimoto@gmail.com>
2024-09-03 20:51:37 -0300

    deleta arquivo e linpa imports

--- src/test/java/com/ecommerce/digital/application/ports/input/UpdateUserByIdInputPortTest.java
+++ src/test/java/com/ecommerce/digital/application/ports/input/UpdateUserByIdInputPortTest.java
@@ -10,7 +10,6 @@ import org.mockito.MockitoAnnotations;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
