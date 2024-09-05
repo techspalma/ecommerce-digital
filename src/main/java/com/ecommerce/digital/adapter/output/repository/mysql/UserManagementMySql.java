628aff26d37d0695089215a1a2d732ff9bd1f265
maran <maranmorimoto@gmail.com>
2024-09-05 09:59:22 -0300

    remocao de try catch

--- src/main/java/com/ecommerce/digital/adapter/output/repository/mysql/UserManagementMySql.java
+++ src/main/java/com/ecommerce/digital/adapter/output/repository/mysql/UserManagementMySql.java
@@ -23,11 +23,7 @@ public class UserManagementMySql implements UserManagementOutputPort {
 
     @Override
     public User findByMail(String email) {
-        try {
-            return userRepository.findByEmail(email).toDomain();
-        } catch (Exception e){
-            return null;
-        } // @PALMA eu sei que isso está errado, mas minha intenção é fazer com que caso nao exista o mail cadastrado no banco ele retorne null. Qual a melhor forma de fazer isso?
+        return userRepository.findByEmail(email).toDomain();
     }
 
     @Override
