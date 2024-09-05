54a5ce0c04f8382e2509c2200531ce0638db9731
maran <maranmorimoto@gmail.com>
2024-09-05 18:21:18 -0300

    adição de logs

--- src/main/java/com/ecommerce/digital/application/ports/input/DeleteUserByIdInputPort.java
+++ src/main/java/com/ecommerce/digital/application/ports/input/DeleteUserByIdInputPort.java
@@ -1,7 +1,10 @@
 package com.ecommerce.digital.application.ports.input;
 
+import com.ecommerce.digital.adapter.input.rest.UserController;
 import com.ecommerce.digital.application.ports.output.UserManagementOutputPort;
 import com.ecommerce.digital.application.useCase.DeleteByUserIdUseCase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -10,8 +13,12 @@ public class DeleteUserByIdInputPort implements DeleteByUserIdUseCase {
     @Autowired
     UserManagementOutputPort userManagement;
 
+    Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @Override
     public void deleteUserById(int id) {
+        logger.info("Início deleção de usuário na camada aplicação");
         userManagement.deleteUserById(id);
+        logger.info("Fim deleção de usuário na camada aplicação");
     }
 }
--- src/main/java/com/ecommerce/digital/application/ports/input/FindUserByIdInputPort.java
+++ src/main/java/com/ecommerce/digital/application/ports/input/FindUserByIdInputPort.java
@@ -3,6 +3,8 @@ package com.ecommerce.digital.application.ports.input;
 import com.ecommerce.digital.application.ports.output.UserManagementOutputPort;
 import com.ecommerce.digital.application.useCase.FindUserByIdUseCase;
 import com.ecommerce.digital.domain.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -13,8 +15,13 @@ public class FindUserByIdInputPort implements FindUserByIdUseCase {
     @Autowired
     UserManagementOutputPort userManagement;
 
+    Logger logger = LoggerFactory.getLogger(FindUserByIdInputPort.class);
+
     @Override
     public Optional<User> findById(int id) {
-        return userManagement.findById(id);
+        logger.info("Início de busca de usuário na camada de aplicação");
+        var user = userManagement.findById(id);
+        logger.info("Fim de busca de usuário na camada de aplicação");
+        return user;
     }
 }
