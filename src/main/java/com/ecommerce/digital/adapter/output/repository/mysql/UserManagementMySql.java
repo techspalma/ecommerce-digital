e46ebd22b1db22db57a52fb9932029bf345c7ca5
maran <maranmorimoto@gmail.com>
2024-09-19 18:55:39 -0300

    adicao de testes findByEmail

--- src/main/java/com/ecommerce/digital/adapter/output/repository/mysql/UserManagementMySql.java
+++ src/main/java/com/ecommerce/digital/adapter/output/repository/mysql/UserManagementMySql.java
@@ -6,7 +6,6 @@ import com.ecommerce.digital.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
